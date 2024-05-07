9c4e9485097544e2a32ee881168cb532c840cab0
RaduBratan <bratanradu7@gmail.com>
2024-05-07 23:55:53 +0300

    Fix all issues, I guess

--- contracts/VotingSystem.sol
+++ contracts/VotingSystem.sol
@@ -10,6 +10,7 @@ contract VotingSystem {
 
     mapping(uint => Candidate) public candidates;
     mapping(address => bool) public voters;
+    mapping(address => bool) public isCandidate;
     uint public candidatesCount;
     address public owner;
 
@@ -23,53 +24,28 @@ contract VotingSystem {
 
     constructor() {
         owner = msg.sender;
-        string[11] memory candidateNames = [
-            "Balenciaga",
-            "Fendi",
-            "Rick Owens",
-            "Maison Margiela",
-            "Comme des Gar\u00E7ons",
-            "Loro Piana",
-            "Brunello Cucinelli",
-            "Corneliani",
-            "Cesare Attolini",
-            "Ermenegildo Zegna",
-            "Berluti"
-        ];
-        for (uint i = 0; i < candidateNames.length; i++) {
-            addCandidate(candidateNames[i]);
-        }
     }
 
     function addCandidate(string memory name) public onlyOwner {
         candidatesCount++;
-        candidates[candidatesCount] = Candidate(candidatesCount, name, 0);
-        emit NewCandidate(candidatesCount, name);
+        uint candidateId = candidatesCount;
+        candidates[candidateId] = Candidate(candidateId, name, 0);
+        isCandidate[msg.sender] = true;
+        emit NewCandidate(candidateId, name);
     }
 
     function vote(uint candidateId) external {
         require(!voters[msg.sender], "Already voted");
-        require(
-            candidateId > 0 && candidateId <= candidatesCount,
-            "Invalid candidate ID"
-        );
+        require(!isCandidate[msg.sender], "Candidates cannot vote");
+        require(candidateId > 0 && candidateId <= candidatesCount, "Invalid candidate ID");
 
         voters[msg.sender] = true;
         candidates[candidateId].voteCount++;
         emit Voted(msg.sender, candidateId);
     }
 
-    function getCandidate(
-        uint candidateId
-    ) external view returns (Candidate memory) {
-        require(
-            candidateId > 0 && candidateId <= candidatesCount,
-            "Invalid candidate ID"
-        );
+    function getCandidate(uint candidateId) external view returns (Candidate memory) {
+        require(candidateId > 0 && candidateId <= candidatesCount, "Invalid candidate ID");
         return candidates[candidateId];
-    }
-
-    function sendEther(address payable recipient) external payable onlyOwner {
-        recipient.transfer(msg.value);
     }
 }
--- test/voting-system.js
+++ test/voting-system.js
@@ -8,6 +8,7 @@ contract("VotingSystem", accounts => {
   });
 
   it("should allow owner to add a candidate", async () => {
+    await votingSystem.addCandidate("Balenciaga", { from: accounts[0] });
     const candidate = await votingSystem.getCandidate(1);
     assert.equal(candidate.name, "Balenciaga", "The name of the first candidate should be Balenciaga.");
   });
@@ -21,15 +22,24 @@ contract("VotingSystem", accounts => {
     }
   });
 
+  it("should not allow candidates to vote", async () => {
+    try {
+      await votingSystem.vote(1, { from: accounts[0] });
+      assert.fail("The candidate was able to vote");
+    } catch (error) {
+      assert.include(error.message, "Candidates cannot vote", "The error message should contain 'Candidates cannot vote'");
+    }
+  });
+
   it("should allow users to vote", async () => {
-    await votingSystem.vote(1, { from: accounts[1] });
+    await votingSystem.vote(1, { from: accounts[2] });
     const candidate = await votingSystem.getCandidate(1);
-    assert.equal(candidate.voteCount.toNumber(), 1, "Candidate should have 1 vote.");
+    assert.equal(candidate.voteCount, 1, "Candidate should have 1 vote.");
   });
 
   it("should not allow users to vote more than once", async () => {
     try {
-      await votingSystem.vote(1, { from: accounts[1] });
+      await votingSystem.vote(1, { from: accounts[2] });
       assert.fail("The user was able to vote more than once");
     } catch (error) {
       assert.include(error.message, "Already voted", "The error message should contain 'Already voted'");
