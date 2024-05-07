cf1ae089417c5d472269fd59cdb91f9465f95ca9
RaduBratan <bratanradu7@gmail.com>
2024-05-07 23:44:50 +0300

    Fix some truffle compile, migrate and test issues

--- truffle-config.js
+++ truffle-config.js
@@ -1,29 +1,17 @@
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
 require('dotenv').config();
 
 module.exports = {
+  migrations_directory: "./migrations",
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*"
     },
-    // ropsten: {
-    //   provider: () =>
-    //     new HDWalletProvider(
-    //       process.env.MNEMONIC,
-    //       `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`
-    //     ),
-    //   network_id: 3, // ID-ul retelei Ropsten
-    //   gas: 5500000, // limita de gaz pentru tranzactii
-    //   confirmations: 2, // Numarul de confirmari de bloc necesare inainte de a considera tranzactiile ca fiind finalizate
-    //   timeoutBlocks: 200, // Numarul de blocuri inainte de a renunta la o tranzactie
-    //   skipDryRun: true // sari peste simularea tranzactiilor inainte de desfasurarea efectiva
-    // }
   },
   compilers: {
     solc: {
-      version: "^0.8.0",
+      version: "0.8.13",
       settings: {
         optimizer: {
           enabled: true,
@@ -32,8 +20,4 @@ module.exports = {
       }
     }
   },
-  // plugins: ["truffle-plugin-verify"],
-  // api_keys: {
-  //   etherscan: process.env.ETHERSCAN_API_KEY
-  // }
 };
